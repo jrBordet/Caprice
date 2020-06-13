316b1ea64fa80a44fd1a8a35d92619e2c81852ef
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-06-13 12:38:02 +0200

    Introduce SyngleTypeComposition

--- Caprice/ForwardApplication.swift
+++ Caprice/ForwardApplication.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 infix operator |>: ForwardApplication
+
 public func |> <A, B> (x: A, f: (A) -> B) -> B {
   return f(x)
 }
---
+++ Caprice/SingleTypeComposition.swift
@@ -0,0 +1,36 @@
+//
+//  SingleTypeComposition.swift
+//  Caprice
+//
+//  Created by Jean Raphael Bordet on 13/06/2020.
+//
+
+import Foundation
+
+precedencegroup SingleTypeComposition {
+    associativity: right
+    higherThan: ForwardApplication
+}
+
+infix operator <>: SingleTypeComposition
+
+public func <> <A>(f: @escaping (A) -> A,
+                   g: @escaping (A) -> A) -> (A) -> A {
+    return f >>> g
+}
+
+public func <> <A>(f: @escaping (inout A) -> Void,
+                   g: @escaping (inout A) -> Void) -> (inout A) -> Void {
+    return { a in
+        f(&a)
+        g(&a)
+    }
+}
+
+public func <> <A: AnyObject>(f: @escaping (A) -> Void,
+                              g: @escaping (A) -> Void) -> (A) -> Void {
+    return { a in
+        f(a)
+        g(a)
+    }
+}
