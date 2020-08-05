384b75f8fb0a98f2ea45f5fae4fbbc3748766afe
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-08-05 10:37:45 +0200

    Introduce KeyPath lenses

--- Caprice/Lenses.swift
+++ Caprice/Lenses.swift
@@ -19,6 +19,31 @@ public struct Lens <A, B> {
     }
 }
 
+// MARK: - KeyPath lenses
+
+public func lens<Whole, Part>(_ keyPath: WritableKeyPath<Whole, Part>) -> Lens<Whole, Part> {
+    return Lens<Whole, Part>(
+        get: { $0[keyPath: keyPath] },
+        set: { part, whole in
+            var copy = whole
+            copy[keyPath: keyPath] = part
+            return copy
+    }
+    )
+}
+
+/**
+ Infix operator of the `set` function.
+ - parameter keyPath: A key path.
+ - parameter part:    A part.
+ - returns: A function that transforms a whole into a new whole with a part replaced.
+ */
+public func *~ <Whole, Part> (keyPath: WritableKeyPath<Whole, Part>, part: Part) -> ((Whole) -> Whole) {
+    return lens(keyPath) *~ part
+}
+
+// MARK: - KeyPath get
+
 public func get<Root, Value>(_ kp: KeyPath<Root, Value>) -> (Root) -> Value {
     return { root in
         root[keyPath: kp]
